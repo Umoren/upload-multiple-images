28952a8b4fa555d441bf8f30cdd609f6499bb617
Umoren <samuelumoren365@gmail.com>
2022-07-04 16:10:35 +0100

    build: init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Col, Row, Image } from 'react-bootstrap'
+import { useState } from 'react'
+import { mockData } from './mock';
 
 function App() {
+
+  const [allImages, setAllImages] = useState([]);
+  const handleClick = async (source) => {
+    const check = allImages.includes(source)
+    if (check) {
+      const id = allImages.indexOf(source)
+      let newArr = allImages
+      newArr.splice(id, 1)
+      setAllImages([...newArr])
+    } else {
+      allImages.push(source)
+      setAllImages([...allImages])
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container>
+      <Row>
+        {mockData.map((data, index) => (
+          <Col md={4} key={index} >
+            <div className="img-card" onClick={() => handleClick(data.src)}>
+              <Image style={{ width: '300px', height: "300px" }} thumbnail src={data.src} />
+              {allImages.includes(data.src) ?
+                <div className="img-checked" >Selected</div>
+                : null}
+            </div>
+          </Col>
+        )
+        )}
+      </Row>
+    </Container>
   );
 }
-
 export default App;
